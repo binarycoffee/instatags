acfd6f15b9ffb5ae1f5f4b308748f35201a0e6cd
william <william@welogy.com>
2017-02-25 06:31:28 -0500

    First commit

---
+++ app/src/main/java/mangoes/instatags/Clarifai.java
@@ -0,0 +1,7 @@
+package mangoes.instatags;
+
+/**
+ * Created by william on 2/25/17.
+ */
+public class Clarifai {
+}
---
+++ app/src/main/java/mangoes/instatags/MainActivity.java
@@ -0,0 +1,167 @@
+package mangoes.instatags;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import clarifai2.api.ClarifaiBuilder;
+import clarifai2.dto.input.ClarifaiInput;
+import clarifai2.dto.model.output.ClarifaiOutput;
+import clarifai2.dto.prediction.Concept;
+
+import static android.view.View.GONE;
+
+public class MainActivity extends AppCompatActivity
+{
+    /**
+     * Absolute path of post image (current)
+     */
+    private String imageAbsolutePath = null;
+
+    /**
+     * Absolute path of post image (new)
+     */
+    private String imageAbsolutePathNew = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Resolve image file path
+     */
+    private File resolveImageFilePath() throws IOException
+    {
+        // Holds timestamp for file name
+        String timeStamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+
+        // Holds filename
+        String filename = "IMAGE_" + timeStamp;
+
+        // Holds empty image file
+        File empty = null;
+
+        // Holds storage directory
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        // Create empty file using new filepath
+        empty = File.createTempFile(filename, ".jpg", storageDir);
+
+        // Set absolute post image path
+        this.imageAbsolutePathNew = empty.getAbsolutePath();
+
+        // Return image
+        return empty;
+    }
+
+    /**
+     * Take image for post
+     */
+    public void takePicture(View view)
+    {
+
+        // Holds image file
+        File imageFile = null;
+
+        // Holds uri file
+        Uri imageURI = null;
+
+        // Create intent to call camera
+        Intent imageIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Attempt to resolve activity for intent
+        if(imageIntent.resolveActivity(getPackageManager()) != null)
+        {
+            // Grab image file
+            try
+            {
+                // Resolve image path
+                imageFile = this.resolveImageFilePath();
+
+                // image uri
+                Log.d("TEST", "over here");
+                imageURI = FileProvider.getUriForFile(this, "mangoes.instatags.ImageStore", imageFile);
+                Log.d("TEST", "here");
+
+                // Set image capturing intent uri resource
+                imageIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageURI);
+
+                // Start image taking activity
+                startActivityForResult(imageIntent,1);
+            }
+            catch(IOException e)
+            {
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        Log.d("CHEASMAE", "result code: " + resultCode);
+        if (resultCode == RESULT_OK){
+            switch(requestCode)
+            {
+                case 1:
+
+                            this.handleImage();
+                    break;
+
+                case 2:
+                    Log.d("CHEASMAE_LOCATION","activity result");
+                    break;
+            }
+        }
+
+    }
+
+    /**
+     * Handle post media photo
+     */
+    private void handleImage()
+    {
+
+        // Create bitmap to contain
+        ImageView takenPicture = (ImageView)findViewById(R.id.pictureTaken);
+
+        Bitmap bitmap = (BitmapFactory.decodeFile(this.imageAbsolutePathNew));
+
+        takenPicture.setImageBitmap(bitmap);
+
+
+        new ClarifaiBuilder("jaxaSxQtMpzIoodkJLLh2Xjtie0G-13VtRxMY-td", "pktFNejsWh11mlELBUrnRuoty6Zh5bXSyGrX1R6d").buildSync();
+        Clarifai.getDefaultModels().generalModel().predict()
+                .withInputs(
+                        ClarifaiInput.forImage(ClarifaiImage.of("https://samples.clarifai.com/metro-north.jpg"))
+                )
+                .executeSync();
+
+
+
+
+    }
+
+
+}
